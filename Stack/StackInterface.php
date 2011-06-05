12722ccee3341d8125b19e904becc15729f17267
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-06-04 20:31:34 -0500

    Add in the stack interface

---
+++ Stack/StackInterface.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Tungsten
+ *-------------------------------------------------------------------
+ * @category    tungsten
+ * @package     tungsten
+ * @author      Damian Bushong
+ * @copyright   (c) 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/tungsten
+ *
+ *===================================================================
+ *
+ */
+
+namespace Codebite\Tungsten\Stack;
+
+/**
+ * Tungsten - Slug parser object interface,
+ * 		Provides the method prototypes that must be defined in all slug parser objects.
+ *
+ *
+ * @category    tungsten
+ * @package     tungsten
+ * @author      Damian Bushong
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/damianb/tungsten
+ */
+interface StackInterface
+{
+	public static function newInstance();
+	public function getStackName();
+	public function parseForStorage($text, &$bitfield, array &$search, array &$replace);
+	public function parseForDisplay($text, &$bitfield, array &$search, array &$replace);
+}
