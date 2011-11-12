19770d9cfe110c81910b9299806042161e90fcee
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-11-12 14:10:06 -0600

    Provide direct access to text parsers, and swap parser loading to use the OpenFlame Framework's dependency injection component.  Fixes #1

--- TextParser.php
+++ TextParser.php
@@ -17,6 +17,7 @@
  */
 
 namespace Codebite\Tungsten;
+use \OpenFlame\Framework\Dependency\Injector;
 
 /**
  * Tungsten - Text parser object,
@@ -31,6 +32,8 @@ namespace Codebite\Tungsten;
  */
 class TextParser
 {
+	const STACK_INJECTOR_PREFIX = 'tungsten.stack.';
+
 	/**
 	 * @var array - Array of slug parser objects to use in the tungsten stack.
 	 */
@@ -41,6 +44,35 @@ class TextParser
 	 */
 	protected $stacks_loaded = array();
 
+	public function __construct()
+	{
+		$injector = Injector::getInstance();
+
+		$injector->setInjector(self::STACK_INJECTOR_PREFIX . 'Image', function() {
+			return new \Codebite\Tungsten\Stack\Image();
+		});
+
+		$injector->setInjector(self::STACK_INJECTOR_PREFIX . 'Link', function() {
+			return new \Codebite\Tungsten\Stack\Link();
+		});
+
+		$injector->setInjector(self::STACK_INJECTOR_PREFIX . 'LinkedImage', function() {
+			return new \Codebite\Tungsten\Stack\LinkedImage();
+		});
+
+		$injector->setInjector(self::STACK_INJECTOR_PREFIX . 'Youtube', function() {
+			return new \Codebite\Tungsten\Stack\Youtube();
+		});
+
+		$injector->setInjector(self::STACK_INJECTOR_PREFIX . 'UniqueSelector', function() {
+			return new \Codebite\Tungsten\Stack\UniqueSelector();
+		});
+
+		$injector->setInjector(self::STACK_INJECTOR_PREFIX . 'TokenRemover', function() {
+			return new \Codebite\Tungsten\Stack\TokenRemover();
+		});
+	}
+
 	/**
 	 * Load a new slug parser object into the tungsten stack if it isn't loaded already.
 	 * @param string $stack_name - The name of the slug parser object to load onto the tungsten stack.
@@ -48,13 +80,28 @@ class TextParser
 	 */
 	public function loadStack($stack_name)
 	{
-		if(!isset($this->stacks_loaded[$stack_name]))
+		$injector = Injector::getInstance();
+		if(!isset($this->stack[$stack_name]))
 		{
-			$stack_class = "\\Codebite\\Tungsten\\Stack\\$stack_name";
-			$this->addStack($stack_class::newInstance());
+			$this->addStack($injector->get(self::STACK_INJECTOR_PREFIX . $stack_name));
 		}
 
 		return $this;
+	}
+
+	/**
+	 * Get a specific parser that's already been loaded.
+	 * @param string $stack_name - The name of the stack parser to grab.
+	 * @return NULL|\Codebite\Tungsten\Stack\StackInterface - The stack parser we wanted, or NULL if not yet loaded.
+	 */
+	public function getStack($stack_name)
+	{
+		if(!isset($this->stack[$stack_name]))
+		{
+			return NULL;
+		}
+
+		return $this->stack[$stack_name];
 	}
 
 	/**
@@ -62,8 +109,7 @@ class TextParser
 	 */
 	protected function addStack(\Codebite\Tungsten\Stack\StackInterface $stack)
 	{
-		$this->stack[] = $stack;
-		$this->stacks_loaded[$stack->getStackName()] = true;
+		$this->stack[$stack::STACK_NAME] = $stack;
 	}
 
 	/**
@@ -80,6 +126,11 @@ class TextParser
 		// Run through the parsing stack and
 		foreach($this->stack as $stack)
 		{
+			if(!$stack->getEnabled())
+			{
+				continue;
+			}
+
 			$search = $replace = array();
 			$count += $stack->parseForStorage($text, $bitfield, $search, $replace);
 			$text = preg_replace($search, $replace, $text, 1);
@@ -89,6 +140,43 @@ class TextParser
 		if($count === 0)
 		{
 			$bitfield = '';
+		}
+
+		return $count;
+	}
+
+	/**
+	 * Parse text for display to the user in an editable format by replacing previously inserted slugs with what was probably the text previously entered.
+	 * @param string &$text - The text to run by the slug parser objects loaded.
+	 * @param string &$bitfield - A randomly generated alphanumeric bitfield string, used to distinguish the slugs in the text.  The bitfield must be stored and provided upon page display to ensure proper slug extraction.
+	 * @return integer - The number of slugs replaced in the text.
+	 * @note This escapes all HTML output for the text itself; no htmlspecialchars on the text is necessary after running it through the display parser.
+	 */
+	public function parseForEdit(&$text, &$bitfield)
+	{
+		$count = 0;
+
+		// Escape output so it can be displayed safely (while the tokens are still unparsed, so they aren't accidentally modified).
+		$text = nl2br(htmlspecialchars($text, ENT_QUOTES, 'UTF-8'), true);
+
+		// If the bitfield is an empty string, there were no tokens inserted into the text when preparing it for storage.
+		// We'll bail out to save some processing here.
+		if(empty($bitfield))
+		{
+			return $count;
+		}
+
+		// Run through the parsing stack and
+		foreach($this->stack as $stack)
+		{
+			if(!$stack->getEnabled())
+			{
+				continue;
+			}
+
+			$search = $replace = array();
+			$count += $stack->parseForEdit($text, $bitfield, $search, $replace);
+			$text = preg_replace($search, $replace, $text, 1);
 		}
 
 		return $count;
@@ -118,6 +206,11 @@ class TextParser
 		// Run through the parsing stack and
 		foreach($this->stack as $stack)
 		{
+			if(!$stack->getEnabled())
+			{
+				continue;
+			}
+
 			$search = $replace = array();
 			$count += $stack->parseForDisplay($text, $bitfield, $search, $replace);
 			$text = preg_replace($search, $replace, $text, 1);
